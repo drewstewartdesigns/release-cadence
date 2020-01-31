05fa747ea53144f4ed3b78870cf8ee30b64aa390
Drew Stewart <Drew_Stewart@anfcorp.com>
2020-01-31 14:26:51 -0500

    TK fixed some shit

--- src/components/actions/actions.component.jsx
+++ src/components/actions/actions.component.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import './actions.styles.css';
 
+const createActions = (actionObj) => {
+    let actions = actionObj.branch.map(branchObj => (
+        <div key={branchObj.id} className={branchObj.class}>
+            <p>{branchObj.name}</p>
+        </div>
+    ));
+    if (actionObj.branch.length > 1) {
+        actions = <div class="nested">{actions}</div>
+    }
+    return actions;
+}
 export const Action = props => (
     <div className='action-container'>
         {props.actions.map(actionObj => (
             <div className={'action-wrapper-' + actionObj.class}>
-            {actionObj.branch.map(branchObj => (
-                <div key={branchObj.id} className={branchObj.class}>
-                    <p>{branchObj.name}</p>
-                </div>
-            ))}
-
-
+            {createActions(actionObj)}
             </div>
         ))}
     </div>

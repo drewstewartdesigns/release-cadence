11cbd1ab6ea4c390b7c4073b43eb8656d52da8f3
Drew Stewart <drewstewartdesigns@gmail..com>
2020-02-05 00:07:09 -0500

    Updates

---
+++ precache-manifest.52e5b269dbb4a353a84961e25db273b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba99d39733d336238ac4ff52988fd7f7",
+    "url": "/release-cadence/index.html"
+  },
+  {
+    "revision": "e4ec7ee1cf64910d6d6b",
+    "url": "/release-cadence/static/css/main.b9207f37.chunk.css"
+  },
+  {
+    "revision": "08748ad7452e8f8c787e",
+    "url": "/release-cadence/static/js/2.1bd93817.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/release-cadence/static/js/2.1bd93817.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e4ec7ee1cf64910d6d6b",
+    "url": "/release-cadence/static/js/main.48e4c9c3.chunk.js"
+  },
+  {
+    "revision": "40734748306930de6fb0",
+    "url": "/release-cadence/static/js/runtime-main.0b7200d2.js"
+  }
+]);
